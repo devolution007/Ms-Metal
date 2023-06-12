ce15d52696ec69de34ebe8117dc2c097c4803f53
Devolution <31021755+devolution007@users.noreply.github.com>
2023-06-12 23:28:43 +0500

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -14,30 +14,23 @@ const Hero = () => {
           backgroundRepeat:'no-repeat',
           backgroundSize:'cover',
           }}>
-              <div className="container">
+            <br></br>
+            <br></br>
+              <div className="container-hero">
                 <div className="-mx-4 flex flex-wrap">
                             <div className="w-full px-4">
               <div
                 className="wow fadeInUp mx-auto max-w-[800px] text-center"
                 data-wow-delay=".2s"
               >
-                {/* <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                <br></br>
+                <br></br>
+                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
                 What is Lorem Ipsum?
                 </h1>
                 <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p> */}
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
+                </p>
                 <br></br>
                 <br></br>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
