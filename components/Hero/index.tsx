f918871ac1895cb7ad43585efac23fd41f6a5084
Devolution <31021755+devolution007@users.noreply.github.com>
2023-06-17 06:11:22 +0500

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -25,14 +25,12 @@ const Hero = () => {
               >
                 <br></br>
                 <br></br>
-                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                What is Lorem Ipsum?
+                <h1 className="mb-5 text-4xl font-bold leading-tight text-black dark:text-black sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                MS | METALS
                 </h1>
-                <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-                <br></br>
-                <br></br>
+                <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-black dark:opacity-90 sm:text-lg md:text-xl" >
+                Building Strength, Forging Excellence                </p>
+
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   {/* <Link
                     href="#"
